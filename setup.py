3f97f0bc3d49f49d69a437ac571ce8d5e31b126e
reedstrm <reedstrm@4837bb00-4500-0410-8bdb-b48e85ea7ec6>
2010-05-12 14:31:42 +0000

    read packaging version from code tree, bump for express-edit

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.67'
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = read("Products", "RhaptosModuleEditor", "version.txt").strip()
 
 setup(name='Products.RhaptosModuleEditor',
       version=version,
