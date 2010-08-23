1dd24a45597a840e106bbfac725e5bef8ca8955d
schatz <schatz@4837bb00-4500-0410-8bdb-b48e85ea7ec6>
2010-08-23 21:55:13 +0000

    Added javascript to search for additional authors on the roles tab. Fixes #10822

--- Products/RhaptosModuleEditor/skins/rhaptos_module_editor/selectfunctions.js
+++ Products/RhaptosModuleEditor/skins/rhaptos_module_editor/selectfunctions.js
@@ -13,3 +13,21 @@ function deselectAllValue(val) {
           checkboxes[i].checked = false ;
        }
 }
+
+/*
+ * When a module is imported (for example through SWORD), there may be additional roles that should
+ * be added to the module. This Javascript will do that.
+ */
+
+function searchUser(name) {
+  var form = document.getElementsByName('collaborators_search')[0];
+  var textbox = null;
+  for (i = 0; i < form.elements.length; i++) {
+    if ('search' == form.elements[i].name) {
+      textbox = form.elements[i];
+    }
+  }
+  textbox.value = name;
+  form.submit();
+}
+
