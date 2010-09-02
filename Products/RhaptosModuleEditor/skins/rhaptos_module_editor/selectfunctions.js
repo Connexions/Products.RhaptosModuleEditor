66a27f3bdc754ae58c73e618f843d1544419213f
schatz <schatz@4837bb00-4500-0410-8bdb-b48e85ea7ec6>
2010-09-02 17:41:51 +0000

    Fixed so search for all creators works. Fixes #10833

--- Products/RhaptosModuleEditor/skins/rhaptos_module_editor/selectfunctions.js
+++ Products/RhaptosModuleEditor/skins/rhaptos_module_editor/selectfunctions.js
@@ -18,16 +18,25 @@ function deselectAllValue(val) {
  * When a module is imported (for example through SWORD), there may be additional roles that should
  * be added to the module. This Javascript will do that.
  */
-
 function searchUser(name) {
   var form = document.getElementsByName('collaborators_search')[0];
-  var textbox = null;
-  for (i = 0; i < form.elements.length; i++) {
-    if ('search' == form.elements[i].name) {
-      textbox = form.elements[i];
-    }
-  }
-  textbox.value = name;
+  var textbox = searchUsersTextbox(form);
+  textbox.value = '"' + name + '"';
   form.submit();
 }
 
+function searchUsers(names) {
+  var form = document.getElementsByName('collaborators_search')[0];
+  var textbox = searchUsersTextbox(form);
+
+  textbox.value = '"' + names.join('", "') + '"';
+  form.submit();
+}
+
+function searchUsersTextbox(form) {
+  for (i = 0; i < form.elements.length; i++) {
+    if ('search' == form.elements[i].name) {
+      return form.elements[i];
+    }
+  }
+}
