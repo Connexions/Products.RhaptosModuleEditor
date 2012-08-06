dd20e1c08c814edd82c225187a5d79957089d4a0
pumazi <pumazi@4837bb00-4500-0410-8bdb-b48e85ea7ec6>
2012-08-06 23:32:10 +0000

    Fixed the upgrade loop and stubbed out the second upgrade.

--- Products/RhaptosModuleEditor/upgrades.py
+++ Products/RhaptosModuleEditor/upgrades.py
@@ -24,6 +24,9 @@ class UpgradeError(Exception):
         Exception(self, message)
         self.reason = reason
 
+def set_version(obj, ver):
+    """Set the module editor version in the object's propertysheet."""
+    setattr(obj, 'editor_version', ver)
 
 def upgrade(module_editor):
     """Upgrade the module editor object"""
@@ -32,12 +35,13 @@ def upgrade(module_editor):
     # Upgrade to the newest version.
     if current_version != NEWEST:
         for i in xrange(current_version, NEWEST):
-            next_version = current_version + 1
-            func = _upgrades[(current_version, next_version)]
+            next_version = i + 1
+            func = _upgrades[(i, next_version)]
             # An UpgradeError will be raised if there are issues.
             logger.debug("Upgrading '%s' from %s to %s" \
-                             % (module_editor, current_version, next_version))
+                             % (module_editor, i, next_version))
             func(module_editor)
+            set_version(module_editor, next_version)
     return True
 
 def upgrade_0_to_1(module_editor):
@@ -51,6 +55,11 @@ def upgrade_1_to_2(module_editor):
     With this change, it can use the dual format (cnxml and html)
     formatting and storage.
     """
-    pass
+    # Run the CNXML to HTML transform
+
+    # TODO Roll over all index.<lang>.cnxml files
+
+    # Store the transformed content to the index.html file
+
 
 _upgrades[(1,2,)] = upgrade_1_to_2
